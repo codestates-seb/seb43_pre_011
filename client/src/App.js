f2af82752864a92524abef71a073cced43ddb1b5
MinhyungEdward <120351766+MinhyungEdward@users.noreply.github.com>
2023-04-21 15:28:26 +0900

    Revert "Feat/layout"

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import Header from "./components/common/Header.jsx";
-import AllQuestions from "./pages/AllQuestions.jsx";
 import { Footer } from "./components/Footer.jsx";
 import GlobalStyle from "./styles/GlobalStyle.js";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <GlobalStyle />
-      <div className="App">
-        <Header />
-        <div className="wrapper">
-          {/* 임시 */}
-          <nav>네비이이이</nav>
-          <div className="container">
-            <AllQuestions />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
