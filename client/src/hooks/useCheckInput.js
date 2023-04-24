9d88cff7d62857f8b40aa32c1cc05f9100bc16db
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-04-24 15:15:37 +0900

    [feat] 로그인 페이지 하드코딩 완료 (#22)
    * feat: 초기세팅 (#5)
    
    * components:에디터
    
    * feat: 푸터 구현 완료 (#6)
    
    * feat: 푸터 구현 완료
    
    * feat: 푸터 구현 완료
    
    * feat: 기본 레이아웃에 헤더 추가
    
    * feat: 기본 컬러 추가
    
    * feat: 공통 링크 스타일 컴포넌트 추가
    
    * feat: 헤더 작성
    
    * UPDATE
    
    * Delete .gradle directory
    
    * Delete .idea directory
    
    * Delete build directory
    
    * components:네비 작업
    
    * components:태그
    
    * 파일 이름 수정
    
    * feat: signUp 구현 중간저장
    
    * 폴더 이름 수정
    
    * Revert "폴더 이름 수정"
    
    * Revert "Feat/layout"
    
    * 파일 이름 수정
    
    * Revert "Revert "Feat/layout""
    
    * 브랜치 수정
    
    * feat: sign up 구현 중간 저장
    
    * feat: Input 컴포넌트 구현
    
    * feat: sign up 페이지 하드 코딩 완료
    
    * feat: Login 페이지 하드코딩 완료
    
    ---------
    
    Co-authored-by: moondrop0816 <87507011+moondrop0816@users.noreply.github.com>
    Co-authored-by: MinhyungEdward <rmaalsgud@gmail.com>
    Co-authored-by: moondrop0816 <moondrop0816@gmail.com>
    Co-authored-by: jae-jung-kim <podosi158@gmail.com>
    Co-authored-by: jae-jung-kim <119948103+jae-jung-kim@users.noreply.github.com>
    Co-authored-by: MinhyungEdward <120351766+MinhyungEdward@users.noreply.github.com>

---
+++ client/src/hooks/useCheckInput.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const useCheckInput = (input, regex) => {
+  const [isError, setIsError] = useState(true);
+  useEffect(() => {
+    if (regex.test(input)) {
+      setIsError(false);
+    } else {
+      setIsError(true);
+    }
+  }, [input, regex]);
+  return isError;
+};
+export default useCheckInput;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,252 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { BtnBlueFill } from "../styles/common.js";
+import { Input } from "../components/Input.jsx";
+import { SignUpText } from "../components/SignUpText.jsx";
+import { SignUpApi } from "../utils/api";
+import useCheckInput from "../hooks/useCheckInput";
+import useInput from "../hooks/useInput";
+// import { useNavigate } from "react-router-dom";
+import BREAKPOINT from "../breakpoint.js";
+
+const Background = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  min-height: calc(100vh - 50px);
+  background-color: #f1f2f3;
+  @media screen and (max-width: ${BREAKPOINT.mobile}) {
+    .side-content {
+      display: none;
+    }
+  }
+  @media screen and (min-width: ${BREAKPOINT.mobile}) {
+    .top-content {
+      display: none;
+    }
+  }
+  .top-content {
+    margin-bottom: 36px;
+    font-size: 21px;
+    text-align: center;
+  }
+  .view-col {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .text {
+      display: flex;
+      margin-bottom: 12px;
+    }
+    .sub-text {
+      font-size: 13px;
+      margin-right: 5px;
+    }
+    .sub-link {
+      font-weight: 500;
+      font-size: 13px;
+      margin-right: 5px;
+      color: #0a95ff;
+    }
+    .icon {
+      fill: #0a95ff;
+    }
+  }
+`;
+const Button = styled(BtnBlueFill)`
+  width: 268px;
+  height: 38.0308px;
+  margin: 12px 0 8px 2px;
+  font-size: 13px;
+  font-weight: 700;
+`;
+
+export const SignUp = () => {
+  // const navigate = useNavigate();
+  const [preventSubmit, setPreventSubmit] = useState(true);
+  const [nickname, nicknameHandler] = useInput("");
+  const [nicknameError, setNicknameError] = useState(false);
+  const [email, emailHandler] = useInput("");
+  const [password, passwordHandler] = useInput("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // nickname, email, password 유효성 검사 코드
+  useEffect(() => {
+    if (nickname === email) {
+      setNicknameError(true);
+    } else {
+      setNicknameError(false);
+    }
+  }, [nickname, email]);
+  const emailError = useCheckInput(
+    email,
+    /[a-zA-Z0-9._]+@[a-zA-Z0-9]+.[a-zA-Z0-9.]+/gm,
+  );
+  const passwordError = useCheckInput(
+    password,
+    /^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$/gm,
+  );
+
+  // 조건별 에러 메시지 저장
+  const emailErrorMessage = () => {
+    if (!email) {
+      return "Email cannot be empty.";
+    }
+    if (emailError) {
+      return `${email} is not a valid email address.`;
+    }
+    return "";
+  };
+
+  // 유효하지 않은 email, password 입력시 제출 방지를 위한 코드
+  useEffect(() => {
+    if (!emailError && !passwordError) {
+      setPreventSubmit(false); // api 통신 활성화를 위한 상태 변경
+    } else {
+      setPreventSubmit(true);
+    }
+  }, [emailError, passwordError]);
+
+  // 제출 버튼 클릭시 이벤트
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitted(true); // 에러 메시지 활성화를 위한 상태변경
+
+    if (preventSubmit) return;
+    const form = {
+      nickname,
+      email,
+      password,
+    };
+    const response = await SignUpApi(form);
+    if (response) {
+      // navigate("/users/login");
+    }
+  };
+
+  return (
+    <Background>
+      <div className="side-content">
+        <SignUpText />
+      </div>
+      <div className="view-col">
+        <h1 className="top-content">
+          Create your Stack Overflow account. It’s free
+          <br /> and only takes a minute.
+        </h1>
+        <form>
+          <Input>
+            <div className="container">
+              <div className="title">
+                Display name
+                <input
+                  type="text"
+                  value={nickname}
+                  onChange={nicknameHandler}
+                  className={
+                    isSubmitted && nicknameError ? "input error" : "input"
+                  }
+                />
+                {isSubmitted && nicknameError ? (
+                  <>
+                    <svg
+                      aria-hidden="true"
+                      className="svg-icon"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                    >
+                      <path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8ZM8 4v6h2V4H8Zm0 8v2h2v-2H8Z"></path>
+                    </svg>
+                    <p className="error-message">
+                      Name and email address must be different. If you
+                      don&apos;t want to enter a name, just leave it blank.
+                    </p>
+                  </>
+                ) : null}
+              </div>
+            </div>
+            <div className="container">
+              <div className="title">
+                Email
+                <input
+                  type="text"
+                  value={email}
+                  onChange={emailHandler}
+                  className={
+                    isSubmitted && emailError ? "input error" : "input"
+                  }
+                />
+                {isSubmitted && emailError ? (
+                  <>
+                    <svg
+                      aria-hidden="true"
+                      className="svg-icon"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                    >
+                      <path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8ZM8 4v6h2V4H8Zm0 8v2h2v-2H8Z"></path>
+                    </svg>
+                    <p className="error-message">{emailErrorMessage()}</p>
+                  </>
+                ) : null}
+              </div>
+            </div>
+            <div className="container">
+              <div className="title">
+                Password
+                <input
+                  type="password"
+                  value={password}
+                  onChange={passwordHandler}
+                  className={isSubmitted && !password ? "input error" : "input"}
+                />
+                {isSubmitted && !password ? (
+                  <>
+                    <svg
+                      aria-hidden="true"
+                      className="svg-icon"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                    >
+                      <path d="M9 17c-4.36 0-8-3.64-8-8 0-4.36 3.64-8 8-8 4.36 0 8 3.64 8 8 0 4.36-3.64 8-8 8ZM8 4v6h2V4H8Zm0 8v2h2v-2H8Z"></path>
+                    </svg>
+                    <p className="error-message">Password cannot be empty.</p>
+                  </>
+                ) : null}
+                <p className="message">
+                  Passwords must contain at least eight characters, including at
+                  least 1 letter and 1 number.
+                </p>
+              </div>
+            </div>
+            <Button onClick={handleSubmit}> Sign up</Button>
+          </Input>
+        </form>
+        <div className="text">
+          <div className="sub-text">Already have an account? </div>
+          <div className="sub-link">Log in </div>
+        </div>
+        <div className="text">
+          <div className="sub-text">Are you an employer? </div>
+          <div className="sub-link">Sign up on Talent </div>
+          <svg
+            aria-hidden="true"
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            className="icon"
+          >
+            <path d="M5 1H3a2 2 0 0 0-2 2v8c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9h-2v2H3V3h2V1Zm2 0h6v6h-2V4.5L6.5 9 5 7.5 9.5 3H7V1Z"></path>
+          </svg>
+        </div>
+      </div>
+    </Background>
+  );
+};
